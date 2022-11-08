35d2835d32e1ac7088462ed339f00b26e7a549d6
Racquel Munoz <racquelg85@hotmail.com>
2022-11-08 17:45:03 -0600

    added index

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -108,3 +108,5 @@ const thoughtController = {
       .catch((err) => res.json(err));
   },
 };
+
+module.exports = thoughtController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,35 +1,6 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-// thought schema
-const ThoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      minlength: 1,
-      maxlength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    reactions: [reactionSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    id: false,
-  }
-);
-
 const reactionSchema = new Schema(
   {
     reactionId: {
@@ -50,6 +21,35 @@ const reactionSchema = new Schema(
       default: Date.now,
       get: (createdAtVal) => dateFormat(createdAtVal),
     },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// thought schema
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
