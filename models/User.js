78ad2e5e277b8f437ccda365822c53e53a0bf2fb
Racquel Munoz <racquelg85@hotmail.com>
2022-11-08 15:05:26 -0600

    created schema

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+// create the User model using the Schema
+const User = model("User", UserSchema);
+
+// get total count of friends
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// export the User model
+module.exports = User;
